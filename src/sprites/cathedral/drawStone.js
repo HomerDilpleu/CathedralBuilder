e18a596ec1b6b99b997ea32fb8c215254891755b
Petit Gigi <petitgigi78@yahoo.fr>
2023-08-23 15:57:52 +0100

    Improve cathdeal building mechanism

--- src/sprites/cathedral/drawStone.js
+++ src/sprites/cathedral/drawStone.js
@@ -6,16 +6,33 @@ game.sprites.cathedral.drawStone = function(ctx, stone) {
     let cube =  game.engine3D.cube
     let spriteHeight = this.height
     let stoneSize = this.stoneSize
-    // Define stone aspect
-    cube.frontStyle = {lineWidth: 1, strokeStyle: 'red', fillStyle: 'blue'}
-    cube.topStyle = {lineWidth: 1, strokeStyle: 'red', fillStyle: 'blue'}
-    cube.bottomStyle = {lineWidth: 1, strokeStyle: 'rgba(0, 0, 0, 0)', fillStyle: 'rgba(0, 0, 0, 0)'}
-    cube.leftStyle = {lineWidth: 1, strokeStyle: 'red', fillStyle: 'blue'}
-    cube.rightStyle = {lineWidth: 1, strokeStyle: 'red', fillStyle: 'blue'}
-    // Update stone aspect dependding on type
-    if (stone.type == 'bottom') {
-        cube.bottomStyle = {lineWidth: 1, strokeStyle: 'red', fillStyle: 'blue'}
+    // Normal stone aspect
+    cube.frontStyle = {lineWidth: 1, strokeStyle: '#876552', fillStyle: '#FFDDCA'}
+    cube.topStyle = {lineWidth: 1, strokeStyle: '#876552', fillStyle: '#FFDDCA'}
+    cube.bottomStyle = {lineWidth: 1, strokeStyle: '#411F0C', fillStyle: '#876552'}
+    cube.leftStyle = {lineWidth: 1, strokeStyle: '876552', fillStyle: '#FFDDCA'}
+    cube.rightStyle = {lineWidth: 1, strokeStyle: '#411F0C', fillStyle: '#876552'}
+    // Ground stone aspect
+    if (stone.type == 'g') {
+        cube.frontStyle = {lineWidth: 1, strokeStyle: '#A39373', fillStyle: '#F3E3C3'}
+        cube.topStyle = {lineWidth: 1, strokeStyle: '#A39373', fillStyle: '#F3E3C3'}
+        cube.bottomStyle = {lineWidth: 1, strokeStyle: '#A39373', fillStyle: '#F3E3C3'}
+        cube.leftStyle = {lineWidth: 1, strokeStyle: '#A39373', fillStyle: '#F3E3C3'}
+        cube.rightStyle = {lineWidth: 1, strokeStyle: '#A39373', fillStyle: '#F3E3C3'}
     }
+    // Roof stone aspect
+    if (stone.type == 'r') {
+        cube.frontStyle = {lineWidth: 1, strokeStyle: '#4E4E4E', fillStyle: '#808080'}
+        cube.topStyle = {lineWidth: 1, strokeStyle: '#4E4E4E', fillStyle: '#808080'}
+        cube.bottomStyle = {lineWidth: 1, strokeStyle: '#4E4E4E', fillStyle: '#808080'}
+        cube.leftStyle = {lineWidth: 1, strokeStyle: '#4E4E4E', fillStyle: '#808080'}
+        cube.rightStyle = {lineWidth: 1, strokeStyle: '#4E4E4E', fillStyle: '#808080'}
+    }
+    
+    // Update stone aspect dependding on type
+    //if (stone.type == 'wb') {
+    //    cube.bottomStyle = {lineWidth: 1, strokeStyle: 'red', fillStyle: 'blue'}
+    //}
     // Define stone size
     cube.size = {x:stoneSize.x, y:stoneSize.y, z:stoneSize.z}
     // Calculate stone coordinates
